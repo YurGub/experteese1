913164ad6422c9d2ed4cd6d285c094bff5bf1ebc
Aleksei <zoneiva@gmail.com>
2016-08-27 14:33:17 +0300

    models migrations + routes - api v1 scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  resources :values
+  resources :images
   root 'main#main'
-  
-  
+   
   #get 'main/main'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      # http://api.experteese.dev/v1/products/1
+      # We are going to list our resources here
+      
+    end
+    
+  end
+  
+  
 end

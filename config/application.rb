48fcc6635efa33f81bac56c37b58e6b1f63b353f
Aleksei <zoneiva@gmail.com>
2016-08-27 16:47:10 +0300

    rspec config

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,22 @@ module Experteese
     config.i18n.fallbacks = true
     config.i18n.fallbacks = [:en]
 
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs true
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config.autoload_paths += %W(\#{config.root}/lib)
+    config.autoload_paths += Dir[Rails.root.join('lib')]
+   # config.autoload_paths += Dir[Rails.root.join('lib', 'added_classes', 'search_work')]
+
+
+
   end
 end

ed8b730e7abd67a440b6a72572bbeb29688ca547
Aleksei <zoneiva@gmail.com>
2016-09-01 13:33:59 +0300

    api ver spec added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'api_constraints'
+  
+  
   resources :values
   resources :images
   root 'main#main'
@@ -10,7 +13,8 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
             constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
       # http://api.experteese.dev/v1/products/1
       # We are going to list our resources here
       

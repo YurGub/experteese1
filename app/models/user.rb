078911a116b6455f973cf7df763d35961f162ec4
Aleksei <blookho@users.noreply.github.com>
2016-09-03 12:52:22 +0300

    User & Micropost model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
+
 end
